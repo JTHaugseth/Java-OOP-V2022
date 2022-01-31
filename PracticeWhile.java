e9fa21970b7358448b77ff57ca1276e844dc6d76
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-01-31 16:57:12 +0100

    Update1

--- PracticeWhile.java
+++ PracticeWhile.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class PracticeWhile {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while(name.isBlank()) {
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.println("Hello "+name);
     }
 }
