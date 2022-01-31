7c7b150b2df2fdf29a193eca4d208c8d58db9683
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-01-31 18:01:25 +0100

    Update

--- Practice2DArrays.java
+++ Practice2DArrays.java
@@ -1,6 +1,40 @@
 public class Practice2DArrays {
 
     public static void main(String[] args) {
-        
+        // 2D Array = an array of arrays
+
+       /* String [][] cars = new String[3][3];
+
+        cars[0][0] = "Camaro";
+        cars[0][1] = "Corvette";
+        cars[0][2] = "Silverado";
+        cars[1][0] = "Mustang";
+        cars[1][1] = "Ranger";
+        cars[1][2] = "F-150";
+        cars[2][0] = "Ferrari";
+        cars[2][1] = "Lamborghini";
+        cars[2][2] = "Tesla";
+
+        for(int i = 0; i < cars.length; i++) {
+            System.out.println();
+            for(int j = 0; j < cars[i].length; j++) {
+                System.out.print(cars[i][j]+" ");
+            }
+        }
+
+        */
+
+        String[][] cars =   {
+                                {"Camaro", "Corvette", "Silverado"},
+                                {"Mustang", "Ranger", "F-150"},
+                                {"Ferrari", "Lambo", "Tesla"}
+                            };
+
+        for(int i = 0; i < cars.length; i++) {
+            System.out.println();
+            for(int j = 0; j < cars[i].length; j++) {
+                System.out.print(cars[i][j]+" ");
+            }
+        }
     }
 }
---
+++ PracticeStringMethods.java
@@ -0,0 +1,4 @@
+public class PracticeStringMethods {
+
+    public static void main()
+}
