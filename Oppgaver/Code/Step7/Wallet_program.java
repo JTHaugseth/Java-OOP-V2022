4583257d7d15a84b16c9a57741e3a91a2c9d8745
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-02-21 19:08:20 +0100

    update

--- Oppgaver/Code/Step7/Wallet_program.java
+++ Oppgaver/Code/Step7/Wallet_program.java
@@ -43,6 +43,7 @@ public class Wallet_program {
         System.out.println("1 - Sjekk hvor mye penger du har");
         System.out.println("2 - Legg til penger");
         System.out.println("3 - Ta ut penger");
+        System.out.println("4 - Avslutt");
 
         boolean repeater = true;
 
@@ -54,16 +55,21 @@ public class Wallet_program {
                         System.out.println("Du har: " + wallet.getCoins() + " kroner");
                         break;
                     case 2:
-                        try{
-                            System.out.println("Hvor mye penger vil du legge til?");
-                            int addCoins = input.nextInt();
-                            wallet.setCoins(wallet.getCoins() + addCoins);
-                            System.out.println("Du legger til "+ addCoins + " kroner og har nå "+ wallet.getCoins()+ " kroner i lommeboken.");
-                        }catch (Exception e) {
-                            System.out.println("you can only write numbers bro!");
-                        }
+                        System.out.println("Hvor mye penger vil du legge til?");
+                        int addCoins = input.nextInt();
+                        wallet.setCoins(wallet.getCoins() + addCoins);
+                        System.out.println("Du legger til "+ addCoins + " kroner og har nå "+ wallet.getCoins()+ " kroner i lommeboken.");
                         break;
                     case 3:
+                        System.out.println("Du har "+ wallet.getCoins() + " kroner. Hvor mye vil du ta ut?");
+                        int removeCoins = input.nextInt();
+                        wallet.setCoins(wallet.getCoins() - removeCoins);
+                        System.out.println("Du tar ut "+ removeCoins + " kroner, og har " + wallet.getCoins() + " kroner igjen i lommeboken");
+                        break;
+                    case 4:
+                        System.out.println("Du går ut av lommeboken");
+                        repeater = false;
+                        break;
                     default:
                         System.out.println("Du kan kun trykke 1,2 eller 3");
 
