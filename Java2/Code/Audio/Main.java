077934fcf03c4599f3cebd029135f23e64efb3a7
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-03-26 15:28:10 +0100

    Audio update

--- Java2/Code/Audio/Main.java
+++ Java2/Code/Audio/Main.java
@@ -3,6 +3,7 @@ package Audio;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -16,8 +17,32 @@ public class Main {
         Clip clip = AudioSystem.getClip();
         clip.open(audioStream);
 
-        clip.start();
+        String response = "";
 
-        String response = scanner.next();
+        while(!response.equals("Q")) {
+            System.out.println("P = play, S = stop, R = reset, Q = quit");
+            System.out.println("Enter your choice");
+
+            response = scanner.next();
+            response = response.toUpperCase();
+
+            switch (response) {
+                case "P":
+                    clip.start();
+                    break;
+                case "S":
+                    clip.stop();
+                    break;
+                case "R":
+                    clip.setMicrosecondPosition(0);
+                    break;
+                case "Q":
+                    clip.close();
+                    break;
+                default:
+                    System.out.println("Not a valid response");
+            }
+        }
+        System.out.println("You quit! Bye :) ");
     }
 }
