3a66d999a6c0bdd43ad80d0be164718f6293b6e2
JTHaugseth <97683128+JTHaugseth@users.noreply.github.com>
2022-05-27 15:33:00 +0200

    update

--- Oppgaver/Code/RepetissionStep17/JDBCOps.java
+++ Oppgaver/Code/RepetissionStep17/JDBCOps.java
@@ -1,9 +1,8 @@
 package RepetissionStep17;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class JDBCOps {
 
@@ -12,15 +11,13 @@ public class JDBCOps {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("The driver registered");
-        }
-        catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
             System.out.println("Could not register driver");
         }
         try {
             createTable();
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("CreateTable failed within jdbcOps");
         }
@@ -53,6 +50,175 @@ public class JDBCOps {
             result = stmt.execute(createTable);
 
         } catch (SQLException sqlException) {
-            sqlException.printStackTrace();}
+            sqlException.printStackTrace();
+        }
+    }
+
+    public ArrayList<Book> getBooks() {
+
+        ArrayList<Book> books = new ArrayList<>();
+
+        try (Connection con = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/booksDb?allowPublicKeyRetrieval" +
+                        "=true&useSSL=false", "julian", "haugseth123")) {
+
+            Statement stmt = con.createStatement();
+
+            String query = "SELECT book.title as title, book.pages as pages, book.genre as genre, author.name as name, author.nationality as nationality" +
+                    "FROM books book" +
+                    "INNER JOIN authors author" +
+                    "ON book.authorId = author.id";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                Book book = new Book();
+                book.setTitle(rs.getString("title"));
+                book.setNumberOfPages(rs.getInt("pages"));
+                book.setGenre(Genre.valueOf(rs.getString("genre")));
+                Author author = new Author();
+                author.setName(rs.getString("name"));
+                author.setNationality(rs.getString("nationality"));
+                book.setAuthor(author);
+                books.add(book);
+            }
+            return books;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public ArrayList<Book> getBooksByAuthor(String authorNameLol) {
+
+        ArrayList<Book> books = new ArrayList<>();
+
+        try (Connection con = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/booksDb?allowPublicKeyRetrieval" +
+                        "=true&useSSL=false", "julian", "haugseth123")) {
+
+            Statement stmt = con.createStatement();
+
+            String query = "SELECT book.title as title, book.pages as pages, book.genre as genre, author.name as name, author.nationality as nationality" +
+                    "FROM books book" +
+                    "INNER JOIN authors author" +
+                    "ON book.authorId = author.id" +
+                    "HAVING author.name = '" +
+                    authorNameLol + "'";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                Book book = new Book();
+                book.setTitle(rs.getString("title"));
+                book.setNumberOfPages(rs.getInt("pages"));
+                book.setGenre(Genre.valueOf(rs.getString("genre")));
+                Author author = new Author();
+                author.setName(rs.getString("name"));
+                author.setNationality(rs.getString("nationality"));
+                book.setAuthor(author);
+                books.add(book);
+            }
+            return books;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public ArrayList<AuthorRecord> getAuthors() {
+
+        ArrayList<AuthorRecord> authors = new ArrayList<>();
+
+        try (Connection con = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/booksDb?allowPublicKeyRetrieval" +
+                        "=true&useSSL=false", "julian", "haugseth123")) {
+
+            Statement stmt = con.createStatement();
+
+            String getTheAuthors = "SELECT * FROM authors";
+
+            ResultSet rs = stmt.executeQuery(getTheAuthors);
+
+            while (rs.next()) {
+                Author author = new Author();
+                author.setName(rs.getString("name"));
+                author.setNationality(rs.getString("nationality"));
+                AuthorRecord authorRecord = new AuthorRecord(rs.getInt("id"), author);
+                authors.add(authorRecord);
+            }
+
+            return authors;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void addBook(Book book) throws SQLException {
+
+        /**
+         * when you add a book, check if the author exists in author table.if not, register author in author table
+         */
+        int authorId;
+        boolean isAuthorPresent = false;
+        ArrayList<AuthorRecord> authorRecords = getAuthors();
+
+        Optional<AuthorRecord> result = (authorRecords!=null && !authorRecords.isEmpty())?
+                authorRecords.stream()
+                        .filter(authorRecord -> authorRecord.getAuthor().equals(book.getAuthor())).findFirst()
+                : Optional.empty();
+        if(result.isPresent()) {
+            authorId = result.get().getTableId();
+            isAuthorPresent = true;
+        } else {
+            authorId = authorRecords.size() + 1;
+        }
+        try (Connection con = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/booksDb?allowPublicKeyRetrieval" +
+                        "=true&useSSL=false", "user1", "pass")) {
+            Statement stmt = con.createStatement();
+            String addBook = "INSERT INTO books(title, authorId, pages, genre)"
+                    + " VALUES('"
+                    + book.getTitle() + "', '"
+                    + authorId + "', '"
+                    + book.getNumberOfPages() + "', '"
+                    + book.getGenre() + "')";
+            stmt.execute(addBook);
+
+            if(!isAuthorPresent ) {
+                String addAuthor = "INSERT INTO authors(name, nationality)"
+                        + " VALUES('"
+                        + book.getAuthor().getName() + "', '"
+                        + book.getAuthor().getNationality()
+                        + "')";
+                stmt.execute(addAuthor);
+            }
+
+        }catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+
+    public void deleteBook(Book book) throws SQLException {
+        try (Connection con = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/booksDb?allowPublicKeyRetrieval" +
+                        "=true&useSSL=false", "user1", "pass")) {
+
+                Statement stmt = con.createStatement();
+                String delete = "DELETE FROM books" +
+                        "WHERE title = '" +
+                        book.getTitle() + "'";
+
+                stmt.executeQuery(delete);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Oppgaver/Code/RepetissionStep17/Main.java
+++ Oppgaver/Code/RepetissionStep17/Main.java
@@ -3,6 +3,14 @@ package RepetissionStep17;
 public class Main {
 
     public static void main(String[] args)  {
+
+        Author author1 = new Author("Julian", "Norge");
+        Author author2 = new Author("Vegard", "Botswana");
+
+        Book book1 = new Book("Book1", author1, 100, Genre.CRIME);
+        Book book2 = new Book("Book2", author2, 300, Genre.CLASSIC);
+        Book book3 = new Book("Book3", author2, 500, Genre.CLASSIC);
+
         JDBCOps jdbcOps = new JDBCOps();
 
     }
